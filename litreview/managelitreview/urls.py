0fcb45baaa42bc2afb83f2f10a0ca32afcd3e03f
DelphinePythonique <delphine.lemire+openclassroom@gmail.com>
2022-06-17 15:37:05 +0200

    add save ticket

--- litreview/managelitreview/urls.py
+++ litreview/managelitreview/urls.py
@@ -3,6 +3,11 @@ from django.urls import path
 
 from . import views
 
+app_name = 'managelitreview'
 urlpatterns = [
     path('home/', views.home, name='home'),
+    path('tickets/new', views.create_ticket, name='create_ticket'),
+    path('tickets/<int:ticket_id>/edit', views.update_ticket, name='update_ticket'),
+    path('tickets/<int:ticket_id>/delete', views.delete_ticket, name='delete_ticket'),
+    path('tickets/<int:ticket_id>', views.display_ticket, name='display_ticket'),
 ]
--- litreview/managelitreview/views.py
+++ litreview/managelitreview/views.py
@@ -1,7 +1,63 @@
 from django.contrib.auth.decorators import login_required
-from django.shortcuts import render
+from django.http import Http404
+from django.shortcuts import render, redirect, get_object_or_404
+
+from . import models
+from .forms import TicketForm
 
 
 @login_required
 def home(request):
-    return render(request, 'managelitreview/home.html')
+    return render(request, "managelitreview/home.html")
+
+
+def _response_new_update_ticket(request, form, type_):
+    if request.method == "POST":
+        if form.is_valid():
+            ticket = form.save(commit=False)
+            ticket.user = request.user
+            ticket.save()
+            return redirect("managelitreview:home")
+
+    return render(request, "managelitreview/create_ticket.html", {"form": form})
+
+
+@login_required
+def create_ticket(request):
+    if request.method == "POST":
+        form = TicketForm(request.POST, request.FILES)
+        if form.is_valid():
+            ticket = form.save(commit=False)
+            ticket.user = request.user
+            ticket.save()
+            return redirect("managelitreview:display_ticket", ticket.id)
+    else:
+        form = TicketForm()
+    return render(request, "managelitreview/create_ticket.html", {"form": form})
+
+
+@login_required
+def update_ticket(request, ticket_id):
+    ticket = get_object_or_404(models.Ticket, id=ticket_id)
+    form = TicketForm(instance=ticket)
+    if request.method == "POST":
+        form = TicketForm(request.POST, request.FILES, instance=ticket)
+        if form.is_valid():
+            form.save()
+            return redirect("managelitreview:display_ticket", ticket_id)
+
+    return render(request, "managelitreview/create_ticket.html", {"form": form})
+
+
+@login_required
+def display_ticket(request, ticket_id):
+    ticket = get_object_or_404(models.Ticket, id=ticket_id)
+
+    return render(request, "managelitreview/display_ticket.html", {"ticket": ticket})
+
+
+@login_required
+def delete_ticket(request, ticket_id):
+    ticket = get_object_or_404(models.Ticket, id=ticket_id)
+    ticket.delete()
+    return redirect("managelitreview:home")
